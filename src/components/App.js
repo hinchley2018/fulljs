3f13ea8b92ea4c5f8d7d784cc7ead736563920f1
jhinchley <dev.jjh@planet-hinchley.com>
2017-10-11 14:02:35 -0500

    refactor: moved data from props to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 
 import ContestPreview from './ContestPreview';
-
+import data from '../testData';
 //assets
 //import '../styles/App.css';
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      pageHeader: 'Naming Contests'
+      pageHeader: 'Naming Contests',
+      contests: []
     };
   }
+  componentDidMount() {
+    this.setState({
+      contests: data.contests
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>{this.state.pageHeader}</h2>
-        {this.props.contests.map(contest =>
-          <ContestPreview {...contest}/>
+        {this.state.contests.map(contest =>
+          <ContestPreview key={contest.id} {...contest}/>
         )}
 
       </div>
