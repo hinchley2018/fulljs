0f2384f8151e922145303828c7b70cbcf2003a40
jhinchley <dev.jjh@planet-hinchley.com>
2017-10-11 15:28:22 -0500

    refactor: moved contests to list component

---
+++ src/components/ContestList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ContestPreview from './ContestPreview';
+import PropTypes from 'prop-types';
+const ContestList = ({contests}) => {
+    return (
+        <div>
+            {contests.map(contest =>
+                <ContestPreview key={contest.id} {...contest}/>
+            )}
+        </div>
+    );
+};
+
+ContestList.propTypes={
+    contests: PropTypes.Object.Array
+};
+
+export default ContestList;
