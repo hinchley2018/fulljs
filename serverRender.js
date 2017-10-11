87daa4903e8e8a8952a50c6c974de495067158ae
jhinchley <dev.jjh@planet-hinchley.com>
2017-10-11 15:01:15 -0500

    refactor: return markup and data

--- serverRender.js
+++ serverRender.js
@@ -10,9 +10,12 @@ import config from './config';
 const serverRender = () =>
   axios.get(`${config.serverUrl}/api/contests`)
     .then(res => {
-      return ReactDOMServer.renderToString(
-        <App initialContests={res.data.contests}/>
-      );
+      return {
+        initialMarkup: ReactDOMServer.renderToString(
+          <App initialContests={res.data.contests}/>
+        ),
+        initialData: res.data
+      };
     });
 
 
